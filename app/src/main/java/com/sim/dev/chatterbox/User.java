dfa8b86f300a9c129dea9551f6feb7505a98570e
ToddlerSam <samsidhu97@gmail.com>
2018-03-31 11:46:00 +0530

    Initial commit

---
+++ app/src/main/java/com/sim/dev/chatterbox/User.java
@@ -0,0 +1,48 @@
+package com.sim.dev.chatterbox;
+
+/**
+ * Created by Sam Sidhu on 11/1/2017.
+ */
+
+public class User {
+
+    String email;
+    String password;
+    String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public User(String email,String password,String name)
+    {
+        this.email=email;
+        this.password=password;
+        this.name=name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
